56a5b18fe2b32d74354cb7ca59e078ac15aac3d9
domtry <doffoufaye@gmail.com>
2023-12-25 10:23:48 +0000

    add admin permission

--- api/users/route.go
+++ api/users/route.go
@@ -2,15 +2,16 @@ package users
 
 import (
 	"auth/middlewares"
+
 	"github.com/labstack/echo/v4"
 )
 
 func RegisterUserRoutes(apiGroup *echo.Group, handler *UserHandler) {
 	apiGroup.GET("", handler.GetAllUsersHandler, middlewares.IsAdminMiddle)
-	apiGroup.POST("", handler.CreateUserHandler)
-	apiGroup.PUT("/:id", handler.UpdateUserHandler)
-	apiGroup.GET("/:id", handler.GetUserByIdHandler)
-	apiGroup.DELETE("/:id", handler.DeleteUserHandler)
-	apiGroup.PUT("/:id/remove-role", handler.RemoveRoleHandler)
-	apiGroup.PUT("/:id/assign-role", handler.AssignRoleHandler)
+	apiGroup.POST("", handler.CreateUserHandler, middlewares.IsAdminMiddle)
+	apiGroup.PUT("/:id", handler.UpdateUserHandler, middlewares.IsAdminMiddle)
+	apiGroup.GET("/:id", handler.GetUserByIdHandler, middlewares.IsAdminMiddle)
+	apiGroup.DELETE("/:id", handler.DeleteUserHandler, middlewares.IsAdminMiddle)
+	apiGroup.PUT("/:id/remove-role", handler.RemoveRoleHandler, middlewares.IsAdminMiddle)
+	apiGroup.PUT("/:id/assign-role", handler.AssignRoleHandler, middlewares.IsAdminMiddle)
 }
